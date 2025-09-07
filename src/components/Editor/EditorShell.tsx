5d2a5289e18abb7d782e86a8b7c6cb844c14bc69
HIMANSHU ANAND <himanshuanand563@gmail.com>
2025-09-07 14:15:40 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/components/Editor/EditorShell.tsx
+++ src/components/Editor/EditorShell.tsx
@@ -41,7 +41,7 @@ export const EditorShell: React.FC<EditorShellProps> = ({
     if (file) {
         setActiveTab(file.id);
     }
-  }, [file, openFiles]);
+  }, [file]);
 
   const handleCloseTab = (e: React.MouseEvent, fileId: string) => {
     e.stopPropagation();
